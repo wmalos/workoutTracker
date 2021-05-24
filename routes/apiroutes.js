754a18075242320721afcd16157139ef8a8f35ed
Will <wmalos22@gmail.com>
2021-05-23 17:11:26 -0700

    added api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,19 +3,61 @@ const { db } = require("../models/workout");
 const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.aggregate( [
-        {
-            $addFields: {
-                totalDuration: { $sum: "$exercises.duration"}
-            }
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+app.get("/api/workouts/range", ({}, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
         },
-    ]).then (data => {
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+      .then((data) => {
+        console.log("workout data", data);
         res.json(data);
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
+        console.log(err);
         res.status(400).json(err);
-    })
-})
+      });
+  });
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+
+      req.params.id,
+      { $push: { exercises: req.body } }
+    )
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -11,9 +11,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/apiroutes.js"));
